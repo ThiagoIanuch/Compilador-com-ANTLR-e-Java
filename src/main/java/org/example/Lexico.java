760562c1899b94a843436b301f56b68a4c44809f
ThiagoIanuch <thiagoianuch@hotmail.com>
2025-11-19 00:32:31 -0300

    Adicionar analisador léxico

---
+++ src/main/java/org/example/Lexico.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lexico extends BaseErrorListener {
+    private List<String> erros = new ArrayList<>();
+
+    public List<String> getErros() {
+        return erros;
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        erros.add("Erro na linha " + line + ", coluna " + charPositionInLine + ": " + msg);
+    }
+}
